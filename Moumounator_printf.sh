aa4c86e7c87a606295966d06318d9eba60257e9b
Mathieu AGOSTINI <magostin@e1r9p3.42.fr>
2020-02-04 03:25:56 +0100

    Working MacOS

--- Moumounator_printf.sh
+++ Moumounator_printf.sh
@@ -1,17 +1,25 @@
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        COLOR_FLAG=\\e
+        ECHO_FLAG=${ECHO_FLAG}
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        COLOR_FLAG=\\x1B
+        ECHO_FLAG=
+fi
+
 echo
-echo -en "\t" ; for i in {16..22} {21..16} {15..22} {21..16} ; do echo -en "\e[38;5;${i}m₪\e[0m" ; done ; echo
-echo -en "\t" ; for i in {16..22} {21..21} ; do echo -en "\e[38;5;${i}m₪\e[0m" ; done ; echo -en "Moumounator" ;
-for i in {21..21} {22..16} ; do echo -en "\e[38;5;${i}m₪\e[0m" ; done ; echo
-echo -en "\t" ; for i in {16..22} {21..20} ; do echo -en "\e[38;5;${i}m₪\e[0m" ; done ; echo -en "FT_PRINTF" ;
-for i in {20..21} {22..16} ; do echo -en "\e[38;5;${i}m₪\e[0m" ; done ; echo
-echo -en "\t" ; for i in {16..22} {21..16} {15..22} {21..16} ; do echo -en "\e[38;5;${i}m₪\e[0m" ; done ; echo
+echo ${ECHO_FLAG} "\t" | tr -d '\n' ; for i in {16..22} {21..16} {15..22} {21..16} ; do echo ${ECHO_FLAG} "${COLOR_FLAG}[38;5;${i}m₪${COLOR_FLAG}[0m" | tr -d '\n'; done ; echo
+echo ${ECHO_FLAG} "\t" | tr -d '\n'; for i in {16..22} {21..21} ; do echo ${ECHO_FLAG} "${COLOR_FLAG}[38;5;${i}m₪${COLOR_FLAG}[0m" | tr -d '\n'; done ; echo ${ECHO_FLAG} "Moumounator" | tr -d '\n';
+for i in {21..21} {22..16} ; do echo ${ECHO_FLAG} "${COLOR_FLAG}[38;5;${i}m₪${COLOR_FLAG}[0m" | tr -d '\n'; done ; echo
+echo ${ECHO_FLAG} "\t" | tr -d '\n'; for i in {16..22} {21..20} ; do echo ${ECHO_FLAG} "${COLOR_FLAG}[38;5;${i}m₪${COLOR_FLAG}[0m" | tr -d '\n'; done ; echo ${ECHO_FLAG} "FT_PRINTF" | tr -d '\n';
+for i in {20..21} {22..16} ; do echo ${ECHO_FLAG} "${COLOR_FLAG}[38;5;${i}m₪${COLOR_FLAG}[0m" | tr -d '\n'; done ; echo
+echo ${ECHO_FLAG} "\t" | tr -d '\n'; for i in {16..22} {21..16} {15..22} {21..16} ; do echo ${ECHO_FLAG} "${COLOR_FLAG}[38;5;${i}m₪${COLOR_FLAG}[0m" | tr -d '\n'; done ; echo
 
 cd your_printf >> /dev/null && make >> /dev/null && mv libftprintf.a .. && cd .. && gcc -w main_all.c gnl/*.c -L. -lftprintf && ./a.out $1 $2
 if [ $? = 1 ]
 then
-  echo -e "\n\t\t\e[1m\e[31m---------------"
+  echo -e "\n\t\t${COLOR_FLAG}[1m${COLOR_FLAG}[31m---------------"
   echo -e "\t\t|ERROR COMPILE|"
-  echo -e "\t\t\e[1m\e[31m---------------\e[0m"
+  echo -e "\t\t${COLOR_FLAG}[1m${COLOR_FLAG}[31m---------------${COLOR_FLAG}[0m"
   echo "Trying new compile command"
   echo
   echo "cd your_printf >> /dev/null && make >> /dev/null && mv *.a .. && cd .. && gcc -w main_all.c gnl/*.c *.a >> /dev/null && ./a.out $1 $2"
@@ -19,12 +27,12 @@ then
 fi
 if [ $? = 1 ]
 then
-  echo -e "\n\t\t\e[1m\e[31m---------------"
+  echo -e "\n\t\t${COLOR_FLAG}[1m${COLOR_FLAG}[31m---------------"
   echo -e "\t\t|ERROR COMPILE|"
-  echo -e "\t\t\e[1m\e[31m---------------\e[0m"
+  echo -e "\t\t${COLOR_FLAG}[1m${COLOR_FLAG}[31m---------------${COLOR_FLAG}[0m"
   echo
   echo "Did you do anything wrong?"
   echo "Refer to README:"
-  echo -e "\e[1m"
+  echo -e "${COLOR_FLAG}[1m"
   cat README
 fi
